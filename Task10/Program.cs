ed4e142dda61d2b8f430be297c2e118eb28e62e2
Чернышев Антон <black2604@mail.ru>
2022-09-06 10:07:21 +0500

    Task10 is complited

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+// выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Напишите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number > 99 & number < 1000) ^ (number < -100 & number > -1000))
+{
+    if (number < -100 & number > -1000)
+    {
+        number *= -1;
+    }
+    number /= 10;
+    number %= 10;
+    Console.WriteLine($"Ваше второе число {number}");
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
