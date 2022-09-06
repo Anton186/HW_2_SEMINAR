0033b7a12d3eb6d2f5001c0b96adb78b907256e1
Чернышев Антон <black2604@mail.ru>
2022-09-06 10:38:21 +0500

    Task13 is complited

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,3 +3,23 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+
+Console.WriteLine("Напишите трёхначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 & number > -99)
+{
+    Console.WriteLine("Тут нету третьей цифры");
+}
+else
+{if (number < -99)
+    {
+        number *= -1;
+    }
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    number %= 10;
+    Console.WriteLine($"Наше число: {number}");
+}
